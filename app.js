5f0797da0c308c609b3e674d974e16b6c01fdd6e
Ugyen Jigten <ujigten@gmail.com>
2017-10-06 01:39:43 +0700

    feat: Add MongoDB and Mongoose

--- app.js
+++ app.js
@@ -1,36 +1,63 @@
-const express = require("express")
-const app = express()
-const bodyParser = require("body-parser")
+const express = require("express"),
+  app = express(),
+  bodyParser = require("body-parser"),
+  mongoose = require("mongoose")
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.set("view engine", "ejs");
+mongoose.connect("mongodb://localhost/tiska")
+app.use(bodyParser.urlencoded({extended: true}))
+app.set("view engine", "ejs")
 
-var props = [
-  { name: "Nike Air Mags", image: "https://mgtvwcmh.files.wordpress.com/2016/10/img_0037.jpg" },
-  { name: "Beauty and the Beast Dress", image: "https://www.dhresource.com/600x600/f2/albu/g5/M00/19/6F/rBVaI1jTjDyAHAOuAAW60_EjGQs999.jpg" }
-]
+// Schema setup
+const propItemSchema = new mongoose.Schema({
+  name: String,
+  image: String,
+  description: String,
+  startingBid: Number,
+})
+
+var PropItem = mongoose.model("PropItem", propItemSchema)
+
+// var props = [
+//   { name: "Nike Air Mags", image: "https://mgtvwcmh.files.wordpress.com/2016/10/img_0037.jpg" },
+//   { name: "Beauty and the Beast Dress", image: "https://www.dhresource.com/600x600/f2/albu/g5/M00/19/6F/rBVaI1jTjDyAHAOuAAW60_EjGQs999.jpg" }
+// ]
 
 app.get("/", function(req, res) {
   res.render("landing")
 })
 
+//  INDEX - show all props
 app.get("/props", function(req, res) {
-  res.render("props", {props})
+  PropItem.find({}, (err, props) => {
+      if(err) {
+        console.log(err)
+      } else {
+        res.render("props", {props})
+      }
+  })
 })
 
 app.get("/props/new", function (req, res) {
-   res.render("new.ejs");
+   res.render("new.ejs")
 });
 
 app.post("/props", function (req, res) {
     // get data from form and add to props array
-    var name = req.body.name;
-    var image = req.body.image;
-    var newProp = {name: name, image: image};
-    props.push(newProp);
-    // redirect back to campgrounds page
-    res.redirect("/props");
-});
+    const name = req.body.name;
+    const image = req.body.image;
+    const description = req.body.description;
+    const startingBid = req.body.startingBid
+    const newProp = {name, image, description, startingBid}
+
+    PropItem.create(newProp, (err, createdProp) => {
+      if(err) {
+        console.log(err)
+      } else {
+        // redirect back to campgrounds page
+        res.redirect("/props")
+      }
+    })
+})
 
 app.listen(3000, () => {
   console.log("Server running on port 3000...")
