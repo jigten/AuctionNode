17ea506b53a40f3b7687570258cad2d77048495a
Ugyen Jigten <ujigten@gmail.com>
2017-10-05 18:01:25 +0700

    chore: Initial setup and commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const app = express()
+const bodyParser = require("body-parser")
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+
+var props = [
+  { name: "Nike Air Mags", image: "https://mgtvwcmh.files.wordpress.com/2016/10/img_0037.jpg" },
+  { name: "Beauty and the Beast Dress", image: "https://www.dhresource.com/600x600/f2/albu/g5/M00/19/6F/rBVaI1jTjDyAHAOuAAW60_EjGQs999.jpg" }
+]
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000...")
+})
